72526c0a014381eb9dc67d529e22fd79486812c0
Tom MacWright <tom@macwright.org>
2015-11-17 17:52:21 -0500

    Add FF test case

--- test.js
+++ test.js
@@ -6,5 +6,10 @@ test('webglHelp', function(t) {
     upgrade: 'http://www.google.com/support/chrome/bin/answer.py?answer=95346',
     troubleshooting: 'http://www.google.com/support/chrome/bin/answer.py?answer=1220892'
   });
+
+  t.deepEqual(webglHelp('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:42.0) Gecko/20100101 Firefox/42.0'), {
+    upgrade: 'http://www.mozilla.com/en-US/firefox/new/',
+    troubleshooting: 'https://support.mozilla.com/en-US/kb/how-do-i-upgrade-my-graphics-drivers'
+  });
   t.end();
 });
